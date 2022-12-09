36c7a7e58e2e493f4ed490ca2a7f7f0ce7ccb4fa
12uminations <nawatpokemon@gmail.com>
2022-12-09 16:06:48 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
 using namespace std;
+int i=0;
+int x=1;
+int N=0;
 
 int main(){
+    while(x !=0){
     cout << "Enter an integer: ";
+    cin >> x;
+    x=x;
+    if(x%2==0){
+        i++;
+    }
+    else{
+        N++;
+    }
+    }
     cout << "#Even numbers = ";
+    cout << i-1 << "\n";
     cout << "#Odd numbers = ";
+    cout << N;
+    
+
     return 0;
 }
